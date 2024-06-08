3b0767d3c31b0bc6e261b6503f00ef5f6190a9cb
C0D3O <1111eugene.eugene@gmail.com>
2024-06-08 18:40:18 +0300

    added swap card

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 
+import { createWeb3Modal, defaultConfig } from '@web3modal/ethers/react';
+import { Button } from '@mui/material';
+
+import { useWeb3Modal } from '@web3modal/ethers/react';
+import { SwapCard } from './components/SwapCard';
+// 1. Get projectId
+const projectId = '6bb7a5f6-9b96-48f9-aef8-d24a655de07d';
+
+// 2. Set chains
+const mainnet = {
+	chainId: 1,
+	name: 'Ethereum',
+	currency: 'ETH',
+	explorerUrl: 'https://etherscan.io',
+	rpcUrl: 'https://cloudflare-eth.com',
+};
+
+// 3. Create a metadata object
+const metadata = {
+	name: 'My Website',
+	description: 'My Website description',
+	url: 'https://mywebsite.com', // origin must match your domain & subdomain
+	icons: ['https://avatars.mywebsite.com/'],
+};
+
+// 4. Create Ethers config
+const ethersConfig = defaultConfig({
+	/*Required*/
+	metadata,
+
+	/*Optional*/
+	enableEIP6963: true, // true by default
+	enableInjected: true, // true by default
+	enableCoinbase: true, // true by default
+	rpcUrl: '...', // used for the Coinbase SDK
+	defaultChainId: 1, // used for the Coinbase SDK
+});
+
+// 5. Create a Web3Modal instance
+createWeb3Modal({
+	// themeVariables: {
+	// 	'--w3m-color-mix': '#00BB7F',
+	// 	'--w3m-color-mix-strength': 40,
+	// 	'--w3m-border-radius-master': '1px',
+	// },
+	ethersConfig,
+	chains: [mainnet],
+	projectId,
+	enableAnalytics: true, // Optional - defaults to your Cloud configuration
+});
+
 export default function App() {
+	const { open } = useWeb3Modal();
 	return (
 		<Container sx={{ minHeight: '100vh', width: '80%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-			<Box>Header</Box>
-			<Box minHeight="90%">
-				<div>hi</div>
-				<div>hi</div>
-				<div>hi</div>
-				<div>hi</div>
-				<div>hi</div>
-				<div>hi</div>
-				<div>hi</div>
-				<div>hi</div>
-				<div>hi</div>
-				<div>hi</div>
-				<div>hi</div>
+			<Box>
+				<Box display="flex" my={1} justifyContent={'flex-end'}>
+					<Button variant="contained" onClick={() => open()} className="btn-wallet__connect">
+						Connect Wallet
+					</Button>
+				</Box>
 			</Box>
-			<Box>Footer</Box>
+			<Box minHeight="90%" sx={{ overflow: 'visible' }}>
+				<SwapCard />
+			</Box>
+			<Box textAlign={'center'} fontSize={12} color={'#514F53'}>
+				{' '}
+				{`DegenDex ${new Date().getFullYear()}`}
+			</Box>
 		</Container>
 	);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import { createRoot } from 'react-dom/client';
-import CssBaseline from '@mui/material/CssBaseline';
-import App from './App';
 import { ThemeProvider } from 'styled-components';
 import { createTheme } from '@mui/material';
+
+import CssBaseline from '@mui/material/CssBaseline';
+
+import App from './App';
 import './index.css';
+
 const root = createRoot(document.getElementById('root')!);
 
 root.render(
 	<>
-		<ThemeProvider theme={createTheme({ palette: { mode: 'dark', text: { primary: '#C7C7C7' } } })}>
+		<ThemeProvider theme={createTheme({ palette: { mode: 'dark', text: { primary: '#C7C7C7' } }, typography: { fontFamily: 'monospace' } })}>
 			{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
 			<CssBaseline />
 			<App />

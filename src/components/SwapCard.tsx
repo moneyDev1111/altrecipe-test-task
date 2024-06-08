969316dd867a3ceccc09133066b5a9dfefce469f
C0D3O <1111eugene.eugene@gmail.com>
2024-06-08 18:40:34 +0300

    added swap card

---
+++ src/components/SwapCard.tsx
@@ -0,0 +1,48 @@
+import { Card, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { useState } from 'react';
+
+const tokens = [
+	{ symbol: 'ETH', decimals: '18', address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' },
+	{ symbol: 'USDT', decimals: '6', address: '0xdAC17F958D2ee523a2206206994597C13D831ec7' },
+	{ symbol: 'USDC', decimals: '6', address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48' },
+	{ symbol: 'DAI', decimals: '18', address: '0x6B175474E89094C44Da98b954EedeAC495271d0F' },
+	{ symbol: 'WBTC', decimals: '8', address: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599' },
+];
+export const SwapCard = () => {
+	const [tokenFrom, setTokenFrom] = useState('ETH');
+	const [tokenTo, setTokenTo] = useState('USDT');
+
+	const mappedTokensFrom = tokens
+		.filter((token) => token.address !== tokenTo)
+		.map((token, index) => (
+			<MenuItem key={index} value={token.symbol}>
+				{token.symbol}
+			</MenuItem>
+		));
+
+	const mappedTokensTo = tokens
+		.filter((token) => token.address !== tokenFrom)
+		.map((token, index) => (
+			<MenuItem key={index} value={token.symbol}>
+				{token.symbol}
+			</MenuItem>
+		));
+
+	return (
+		<Card sx={{ padding: '1em', display: 'flex', flexDirection: 'column', width: '20%', margin: '0 auto', gap: '2vh' }}>
+			<FormControl>
+				<InputLabel id="from-select-label">From</InputLabel>
+				<Select labelId="from-select-label" value={tokenFrom} label="From" onChange={(e) => setTokenFrom(e.target.value)}>
+					{mappedTokensFrom}
+				</Select>
+			</FormControl>
+
+			<FormControl>
+				<InputLabel id="to-select-label">To</InputLabel>
+				<Select labelId="to-select-label" value={tokenTo} label="To" onChange={(e) => setTokenTo(e.target.value)}>
+					{mappedTokensTo}
+				</Select>
+			</FormControl>
+		</Card>
+	);
+};
